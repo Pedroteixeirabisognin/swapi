ae9a4b639e3e8bd86ec79dc1f3a94261919deeb7
Pedro Bisognin <pedroteixeirabisognin@gmail.com>
2026-09-20 02:42:41 -0300

    Início do projeto

---
+++ src/test/java/br/com/pedro/swapi/SwapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pedro.swapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
